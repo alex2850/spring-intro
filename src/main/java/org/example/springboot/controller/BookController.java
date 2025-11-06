a64b6b803c7eb31239803ac95f7dade9a7284156
Oleksandr <odombrovskyi26@gmail.com>
2025-11-07 00:09:01 +0300

    last fix

--- src/main/java/org/example/springboot/controller/BookController.java
+++ src/main/java/org/example/springboot/controller/BookController.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-@Tag(name = "Book management", description = "Endpoints for maneging books")
+@Tag(name = "Book management", description = "Endpoints for managing books")
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/books")
